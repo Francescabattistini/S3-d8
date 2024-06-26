60142bfbf1bea892e78e04f830330f9a07cf42e5
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-06-26 18:00:59 +0200

    alien

--- js/javascrip.js
+++ js/javascrip.js
@@ -3,11 +3,13 @@ const taskMaker = document.getElementById("task-maker");
 // logica per la to-do list e per il task-maker
 taskMaker.addEventListener("submit", function (event) {
   event.preventDefault();
+  // ho preso gli elementi
   const customTask = document.getElementById("custom-task");
   const list = document.getElementById("list");
-  // create elements
+  // ho creato gli elementi
   const div = document.createElement("div");
   const check = document.createElement("input");
+  //definisco il comportamento della check
   check.type = "checkbox";
   check.addEventListener("click", function () {
     check.parentNode.classList.add("fade-out");
@@ -15,14 +17,18 @@ taskMaker.addEventListener("submit", function (event) {
       check.parentNode.remove();
     }, 1000);
   });
+
+  // creo elemento p e ci aggiungo la classe task
   const task = document.createElement("p");
   task.classList.add("task");
+  //creo la linea e la disabilito al click
   task.addEventListener("click", function (event) {
     task.classList.toggle("crossed");
   });
   // rende la prima lettera della task maiuscola di default
   const upperCase =
-    customTask.value.slice(0, 1).toUpperCase() + customTask.value.slice(1);
+    customTask.value.slice(0, 1).toUpperCase() + customTask.value.slice(1); // seleziono la prima lettera a cui fare la maiuscola ,
+  // ma poi mi toglie tutte le altre quindi gli dico dalla prima aggiungi il resto
   task.innerText = upperCase;
   // mette sulla stessa linea il checkbox e la task
   check.classList.add("inline");
@@ -34,5 +40,3 @@ taskMaker.addEventListener("submit", function (event) {
   //reset form
   taskMaker.reset();
 });
-
-const header = document.getElementsByTagName("header")[0];
